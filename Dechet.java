322ded6ab6a446556e333d64f243c7afe72130c1
dahhou147 <126627013+dahhou147@users.noreply.github.com>
2023-04-01 04:10:30 +0200

    Update Dechet.java

--- Dechet.java
+++ Dechet.java
@@ -1,4 +1,24 @@
 
 public class Dechet {
-
+   private String type;
+	private double poids;
+	private Poubelle poubelleContientDechet;
+	private depot depot;
+	private ArrayList<String> listeTypeDechet= new ArrayList<>();
+	
+	public String gettype(){
+		return type;
+	}
+	public double getpoids() {
+		return poids;
+	}
+	public Poubelle getpoubelleContientDechet() {
+		return poubelleContientDechet;
+	}
+	 public depot getdepot() {
+		 return depot;
+	 }
+	 public ArrayList<String> getlisteTypeDechet(){
+		 return listeTypeDechet;
+	 }
 }
