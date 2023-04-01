748ec4a556d1c766681def736da113bc8db90071
dahhou147 <126627013+dahhou147@users.noreply.github.com>
2023-04-01 04:12:26 +0200

    Update Depot.java

--- Depot.java
+++ Depot.java
@@ -1,4 +1,39 @@
 
 public class Depot {
+    private int id;
+    private int menage;
+    private int poubelle;
+    private Date heure = new Date();
+    private double quantiteDeDechet;
+    private ArrayList<String> dechetDepose = new ArrayList<>();
+    private long suivi_identifiant;
+    
+    public int getId() {
+        return id;
+    }
+
+    public int getMenage() {
+        return menage;
+    }
+
+    public int getPoubelle() {
+        return poubelle;
+    }
+
+    public Date getHeure() {
+        return heure;
+    }
+
+    public double getQuantiteDeDechet() {
+        return quantiteDeDechet;
+    }
+
+    public ArrayList<String> getDechetDepose() {
+        return dechetDepose;
+    }
+
+    public long getSuiviIdentifiant() {
+        return suivi_identifiant;
+    }
 
 }
