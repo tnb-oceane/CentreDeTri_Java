5521019085817aa71cc1f38f0164e2bf74ecafa1
bilallyy <128432862+bilallyy@users.noreply.github.com>
2023-04-07 19:58:48 +0200

    Update TestBon.java

--- TestBon.java
+++ TestBon.java
@@ -7,7 +7,11 @@ public class TestBon {
 
 	public static void main(String[] args) {
 		// Créer un commerce
-		Commerce commerce1 = new Commerce("COM1", "Boulangerie");
+		
+		ArrayList<String> listeVide = new ArrayList<>();
+		Commerce abc123 = new Commerce("ABC123", listeVide);
+
+
 		
 		// Créer une liste de produits concernés
 		ArrayList<String> produitsConcernés = new ArrayList<>();
@@ -19,7 +23,7 @@ public class TestBon {
 		Date dateLimite = new Date(dateEmission.getTime() + (1000 * 60 * 60 * 24 * 7)); // Date limite dans une semaine
 		
 		// Créer un bon
-		Bon bon1 = new Bon(TypeBon.REDUCTION, 5, 10, commerce1, dateEmission, dateLimite, produitsConcernés, true);
+		Bon bon1 = new Bon(TypeBon.REDUCTION, 5, 10, abc123, dateEmission, dateLimite, produitsConcernés, true);
 		
 		// Afficher les informations du bon
 		System.out.println(bon1.toString());
